a1d628fe182d4a2b4f20432a187878eae92f4258
AzemaMaksatova <sanzu18haru@gmail.com>
2026-02-05 00:49:50 +0600

    hwhw

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,19 @@
-import { View, Button, StyleSheet } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function HomeScreen() {
+export default function App() {
   return (
     <View style={styles.container}>
-      <Button title="Azema" onPress={() => {}} />
+      <View style={styles.overlay}>
+        <Text style={styles.name}>AZEMA</Text>
+        <Text style={styles.course}>React Native</Text>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {}}
+        >
+          <Text style={styles.buttonText}>KNOPKA</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -11,7 +21,33 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#1e1e1e',
+  },
+  overlay: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  name: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 10,
+  },
+  course: {
+    fontSize: 18,
+    color: '#ccc',
+    marginBottom: 25,
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
   },
 });
